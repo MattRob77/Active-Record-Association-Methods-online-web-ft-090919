f1e095f6cbe24cee0993953739b2bfcfeff0d05b
Matthew Robinson <matt_robinson1@yahoo.com>
2019-10-16 14:32:38 +0000

    Automatically backed up by Learn

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -3,14 +3,14 @@ class Artist < ActiveRecord::Base
   has_many :genres, through: :songs
 
   def get_genre_of_first_song
-    self.get_first_song  
+    self.songs.first.genre
   end
 
   def song_count
-    self.songs.length 
+    self.songs.length
   end
 
   def genre_count
-    self.genres.length 
+    self.genres.length
   end
 end
